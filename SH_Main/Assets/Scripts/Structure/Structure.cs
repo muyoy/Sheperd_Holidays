15e7451fbda055dba2d39467e5a0786623bdbae0
Sherlock517 <49934162+Sherlock517@users.noreply.github.com>
2020-11-06 18:13:25 +0900

    Sub Edit

---
+++ SH_Main/Assets/Scripts/Structure/Structure.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour
+{
+    protected int hitPoints;
+    public int HP{
+        get { return hitPoints; }
+        set {
+            hitPoints = value;
+        }
+    }
+
+    public virtual bool buildCheck(){ return true; }  // 건물이 지어졌는지 체크
+
+    protected int cost;
+    protected float buildTime; // 건물이 지어질 때 필요한 시간
+    protected int buildingSpace; // 건물이 지어질 때 필요한 공간
+
+    protected virtual void buildingFunc(){}
+    protected virtual float HpChange(float damage){ return 0f; }
+
+}
