c8e4c7718d1d4cee4afea7fb6f6d004d7e163915
muyoy <48443092+muyoy@users.noreply.github.com>
2020-11-06 16:55:39 +0900

    stack, List 수정

--- SH_Main/Assets/Scripts/BattleManager.cs
+++ SH_Main/Assets/Scripts/BattleManager.cs
@@ -9,13 +9,17 @@ public class BattleManager : MonoBehaviour
     public GameObject farm, Forest;
     public List<GameObject> sheeps = new List<GameObject>();
     public List<GameObject> wolfs = new List<GameObject>();
-    public Stack<GameObject> walls = new Stack<GameObject>();
 
+    private Stack<GameObject> walls = new Stack<GameObject>();
     private Vector2 wolf_way, sheep_way;
     private RaycastHit2D farm_ray, forest_ray;
 
-    //test용
+
+    /// <summary>
+    /// 테스트용 오브젝트
+    /// </summary>
     public GameObject wolf, sheep;
+
     private void Start()
     {
         wolf_way = Vector2.left;
@@ -52,22 +56,43 @@ public class BattleManager : MonoBehaviour
         }
     }
 
-    public void UnitAdd(GameObject unit)
+    #region UnitSetting <List>
+    public void AddUnit(GameObject unit)
     {
-        if(unit.layer == sheep_layer)
-        {
+        if (unit.layer == sheep_layer)
             sheeps.Add(unit);
-        }
         else
-        {
             wolfs.Add(unit);
-        }
     }
+
+    public void RemoveUnit(GameObject unit)
+    {
+        if (unit.layer == sheep_layer)
+            sheeps.Remove(unit);
+        else
+            wolfs.Remove(unit);
+    } 
+    #endregion
+
+    #region WallSetting <Stack>
+
     public void SetWall(GameObject wall)
     {
         walls.Push(wall);
     }
+    public GameObject GetWall()
+    {
+        if (walls.Count != 0)
+            return walls.Peek();
+        else
+            return null;
+    }
+    public void RemoveWall(GameObject wall)
+    {
+        walls.Pop();
+    }
 
+    #endregion
     //test용
     private void Update()
     {
