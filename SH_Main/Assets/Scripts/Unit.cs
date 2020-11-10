b525b65197b3e538d34354064cf97539b3deca00
muyoy <48443092+muyoy@users.noreply.github.com>
2020-11-10 10:32:18 +0900

    scirpt

---
+++ SH_Main/Assets/Scripts/Unit.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public enum Kind { Sheep, Wolf}
+    public enum Type { Short, Middle, Long }
+
+    [SerializeField]
+    protected Kind kind;
+    [SerializeField]
+    protected Type type;
+
+    [SerializeField]
+    protected float hp;
+    protected virtual float Hp
+    {
+        get { return hp; }
+        set
+        {
+            hp = value;
+        }
+    }
+    [SerializeField]
+    protected Vector2 targetPos;
+    protected GameObject atkTarget = null;
+    protected int atk = 0;
+    protected int speed = 10;
+    protected float range = 0;
+    protected float atk_cool = 0.0f;
+
+    public bool isDead;
+    public BattleManager BM;
+
+    public virtual void HpChanged(float damage)
+    {
+        if(!isDead)
+        {
+            Hp -= damage;
+        }
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+
+    protected virtual void Move()
+    {
+        targetPos = BM.GetWall().transform.position;
+    }
+}
