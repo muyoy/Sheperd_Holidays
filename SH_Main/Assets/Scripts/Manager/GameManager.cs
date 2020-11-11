c9d8533bb3d54b29958047fb413b28dfec7d99f4
CorgiMuzi <tomejerry99@gmail.com>
2020-11-11 20:57:41 +0900

    ,jhmgvhjmgv

--- SH_Main/Assets/Scripts/Manager/GameManager.cs
+++ SH_Main/Assets/Scripts/Manager/GameManager.cs
@@ -3,31 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public struct GroundInfo
-{
-    public Image ground;
-    public bool IsEmpty;
-
-    public GroundInfo(Image image, bool tf)
-    {
-        ground = image;
-        IsEmpty = tf;
-    }
-};
 
 
 public class GameManager : MonoBehaviour
 {
-    private bool IsDay; // 아침인지 밤인지 구분하는 bool
-    public GameObject MorningMap; // 아침 맵
-    public GameObject EveningMap; // 저녁 맵
-    private List<GroundInfo> morningGround;
-    private List<GroundInfo> eveningGround;
+    public bool IsDay; // 아침인지 밤인지 구분하는 bool
+    
 
     private void Awake()
     {
-        morningGround = new List<GroundInfo>();
-        eveningGround = new List<GroundInfo>();
+        
 
         IsDay = false; // 밤으로 게임 시작
     }
@@ -37,43 +22,9 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void MapGetter() // Map 게임오브젝트에 들어있는 Image를 리스트에 삽입
-    {
-        for (int i = 0; i < MorningMap.transform.childCount; i++)
-        {
-            morningGround.Add(new GroundInfo(MorningMap.transform.GetChild(i).GetComponent<Image>(), true));
-        }
-        for (int i = 0; i < EveningMap.transform.childCount; i++)
-        {
-            eveningGround.Add(new GroundInfo(EveningMap.transform.GetChild(i).GetComponent<Image>(), true));
-        }
-    }
+    
 
-    private void GroundSet()
-    {
-        if (IsDay)
-        {
-            for (int i = 0; i < morningGround.Count; i++)// 아침이면 아침 땅 활성화
-            {
-                morningGround[i].ground.gameObject.SetActive(true);
-            }
-            for (int i = 0; i < eveningGround.Count; i++)
-            {
-                eveningGround[i].ground.gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < morningGround.Count; i++)// 밤이면 아침 땅 비활성화
-            {
-                morningGround[i].ground.gameObject.SetActive(false);
-            }
-            for (int i = 0; i < eveningGround.Count; i++)
-            {
-                eveningGround[i].ground.gameObject.SetActive(true);
-            }
-        }
-    }
+    
 
     
 }
