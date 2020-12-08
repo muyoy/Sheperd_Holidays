24db13b0eb02d2cfec9eb9c59c2221fb5b0aac6f
djfdmator <43840175+djfdmator@users.noreply.github.com>
2020-12-09 02:09:18 +0900

    Implement Building Windmill

--- SH_Main/Assets/Scripts/Structure/Resource/WindMill.cs
+++ SH_Main/Assets/Scripts/Structure/Resource/WindMill.cs
@@ -4,18 +4,122 @@ using UnityEngine;
 
 public class WindMill : Structure
 {
-    public GameObject windmillWing;
+    [Space(1.0f)]
+    [Header("Windmill Parameter")]
+    public int Level = 0;
+    public enum State { Build, Product, Destroy };
+    public State state;
+    public int resource = 0;
+    public float resourceCreateTime = 5.0f;
+    public bool isProduction = false;
+
+    public GameObject[] BuildingLevel;
+
+    private Coroutine resourceCreate = null;
+
     public override void Init()
     {
         base.Init();
-        windmillWing.SetActive(true);
+        StartCoroutine(BuildStructure());
     }
 
-    private void Update()
+    IEnumerator BuildStructure()
     {
-        float angle = 0;
-        
-        windmillWing.transform.Rotate(new Vector3(0, 0, angle));
-        angle += 1f;
+        state = State.Build;
+        StopResourceCreate();
+        float time = 0.0f;
+#if UNITY_EDITOR
+        Debug.Log("건물 지을때의 이펙트 필요!!");
+#endif
+        while (time <= buildTime)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+#if UNITY_EDITOR
+        Debug.Log("건물 공사 완료시 이펙트 필요!!");
+#endif
+        yield return null;
+        Level++;
+        ChangeBuildingImage();
+        resourceCreate = StartCoroutine(ResourceCreate());
+    }
+
+    IEnumerator ResourceCreate()
+    {
+        state = State.Product;
+        isProduction = true;
+        float time = 0.0f;
+        while (isProduction)
+        {
+            time += Time.deltaTime;
+            if (resourceCreateTime <= time) resource = CreateResource();
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    public void BuildButton()
+    {
+        if (state == State.Build) return;
+
+        StartCoroutine(BuildStructure());
+    }
+
+    public void ResourceGathering()
+    {
+        //TODO : DB += resource * factor
+        resource = 0;
+    }
+
+    private int CreateResource()
+    {
+        switch(Level)
+        {
+            case 1: return 1;
+            case 2: return 2;
+            case 3: return 4;
+            default: return 0;
+        }
+    }
+
+    private void ChangeBuildingImage()
+    {
+        switch (Level)
+        {
+            case 1:
+                BuildingLevel[0].SetActive(true);
+                break;
+            case 2:
+                BuildingLevel[0].SetActive(false);
+                BuildingLevel[1].SetActive(true);
+                break;
+            case 3:
+                BuildingLevel[1].SetActive(false);
+                BuildingLevel[1].SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void StopResourceCreate()
+    {
+        if (resourceCreate != null)
+        {
+            StopCoroutine(resourceCreate);
+            resourceCreate = null;
+        }
+    }
+
+    protected override int HpChange(float damage)
+    {
+        return base.HpChange(damage);
+    }
+
+    protected override void BuildingFunc()
+    {
+        base.BuildingFunc();
     }
 }
