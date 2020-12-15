41c4b8851ff82c62d4057df78efacefacfae8043
djfdmator <43840175+djfdmator@users.noreply.github.com>
2020-12-16 00:58:37 +0900

    inputmanager

---
+++ SH_Main/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            // Bit shift the index of the layer (8) to get a bit mask
+            //int layerMask = 1 << 8;
+
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            //layerMask = ~layerMask;
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, out hit, 20.0f))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                hit.transform.gameObject.SendMessage("OnClickEvent");
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20.0f, Color.white);
+                Debug.Log("Did not Hit");
+            }
+        }
+    }
+}
