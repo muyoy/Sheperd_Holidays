f086e0bf432bdbec2afb4b16834060e6b7556abb
tpdms025 <45121963+tpdms025@users.noreply.github.com>
2021-01-04 17:52:54 +0900

    하늘 회전, 산 배경 변경

---
+++ SH_Main/Assets/Scripts/Map/SkyRotation.cs
@@ -0,0 +1,30 @@
+﻿// ==============================================================
+// 하늘 회전
+//
+// AUTHOR: Yang SeEun
+// CREATED: 2020-12-29
+// UPDATED: 2020-12-29
+// ==============================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyRotation : MonoBehaviour
+{
+    private float time = 0;
+
+    public IEnumerator Rotation(float angle, float duration)
+    {
+        Quaternion from = transform.rotation;
+        while (time <= duration)
+        {
+            time += Time.deltaTime;
+            //Lerp 작동안됨..
+            //transform.rotation = Quaternion.Lerp(from, targetRotation, time/duration);
+            transform.rotation = Quaternion.Euler(0,0, Mathf.LerpAngle(0,-angle, time / duration));
+            yield return null;
+        }
+        time = 0;
+    }
+}
