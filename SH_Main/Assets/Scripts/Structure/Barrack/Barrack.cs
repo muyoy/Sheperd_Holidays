5668dd8657b40c8ad56bbe389a37ca25faf4fcf9
CorgiMuzi <tomejerry99@gmail.com>
2021-01-08 13:36:19 +0900

    backup

--- SH_Main/Assets/Scripts/Structure/Barrack/Barrack.cs
+++ SH_Main/Assets/Scripts/Structure/Barrack/Barrack.cs
@@ -1,7 +1,7 @@
 ﻿//************************************************************
 //
 //  EDITOR : KIM JIHUN
-//  LAST UPDATE : 2020.12.24
+//  LAST UPDATE : 2021.1.6
 //  Script Purpose :  Fundamental Script of All Barracks
 //  하단 인터페이스에서 유닛 생성 버튼을 누르면 유닛 생산 대기열을 나타나게 하고 시간을 표시, 시간이 다 되면 유닛 생성
 //
@@ -33,15 +33,113 @@ public class Barrack : Structure
 
     public List<UnitInfo> UnitList = new List<UnitInfo>();  // 생산 가능한 유닛들의 리스트
     public GameObject UIManager;
+    public GameObject[] UnitQueue;
     private UIManager UI;
-    
+    public GameObject[] GenUnitImg;
+    public Queue<GenUnitInfo> GenUnitQueue = new Queue<GenUnitInfo>();
+    private float timer;
+    private bool IsGenerating = false; // 지금 유닛을 생산하고 있는가?
     private bool isActive = true; // 지금 Barrack을 클릭한 상태인가?
+
+    #region Coroutines
+    public Coroutine UnitProductionCoroutine = null;
+    #endregion Coroutines
+
 
     private void Awake()
     {
         UIManager = GameObject.Find("UIManager");
         UI = UIManager.GetComponent<UIManager>();
+
+        GenUnitImg = UI.UnitRepIcon;
+        InitUnitQueue();
     }
 
-    
+    private void Start()
+    {
+        Init();
+        for (int i = 0; i < GenUnitImg.Length; i++)
+        {
+            GenUnitImg[i].GetComponent<UnitGenerate>().TargetBarrack(gameObject);
+        }
+
+    }
+
+    private void InitUnitQueue()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            UnitQueue[i] = transform.GetChild(i).gameObject;
+            UnitQueue[i].SetActive(false);
+        }
+    }
+
+
+    public IEnumerator UnitProduction() // 유닛 생산시간 관리
+    {
+        DBStruct.SheepData GeneratingUnit;
+        while (GenUnitQueue.Count > 0)
+        {
+#if UNITY_EDITOR
+            Debug.Log("GenUnitQueue : " + GenUnitQueue.Count);
+            Debug.Log("UnitProduction : " + UnitProductionCoroutine);
+#endif
+            GeneratingUnit = GenUnitQueue.Peek().sheepData;
+            IsGenerating = true;
+            StartCoroutine(Clock());
+
+            // Clock 스타트?
+
+            while (timer < GeneratingUnit.waitingTime) {
+#if UNITY_EDITOR
+                Debug.Log("timer : " + timer);
+                Debug.Log("Target Time : " + GeneratingUnit.waitingTime);
+#endif 
+                yield return null; 
+            }
+            if (timer >= GeneratingUnit.waitingTime)
+            {
+#if UNITY_EDITOR
+                Debug.Log("if 문 실행");
+#endif 
+                StopCoroutine(Clock());
+                InstantiateUnit(GeneratingUnit);
+                for (int i = 0; i < GenUnitQueue.Count; i++)
+                {
+                    if (i < GenUnitQueue.Count - 1 && i>=0)
+                    {
+                        UnitQueue[i].GetComponent<SpriteRenderer>().sprite = UnitQueue[i + 1].GetComponent<SpriteRenderer>().sprite;
+                    }
+                    else
+                    {
+                        UnitQueue[i].SetActive(false);
+                    }
+                }
+            }
+
+            if(GenUnitQueue.Count <= 0)
+            {
+                UnitProductionCoroutine = null;
+                StopCoroutine(UnitProduction());
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+
+    private IEnumerator Clock()
+    {
+        timer = 0;
+        while (IsGenerating)
+        {
+            timer++;
+            yield return new WaitForSeconds(1);
+        }
+        yield return null;
+    }
+
+    private void InstantiateUnit(DBStruct.SheepData sheep)
+    {
+
+    }
 }
---
+++ SH_Main/Assets/Scripts/Structure/Barrack/UnitGenerate.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct GenUnitInfo{
+    public DBStruct.SheepData sheepData;
+    public Sprite sheepImage;
+
+    public GenUnitInfo(DBStruct.SheepData sheep, Sprite sprite)
+    {
+        sheepData = sheep;
+        sheepImage = sprite;
+    }
+}
+
+public class UnitGenerate : MonoBehaviour
+{
+    public enum UnitType { Sword = 0, Bow, Javelin }
+    private Barrack barrackFunc;
+    public UnitType unitType;
+
+    private void Awake()
+    {
+
+    }
+
+    public void TargetBarrack(GameObject barrack) // 배럭이 생성되거나 파괴되고 난 후에 다시 배럭을 지정해줘야 할 때 사용
+    {
+        barrackFunc = barrack.GetComponent<Barrack>();
+    }
+
+    public void AddUnit()
+    {
+        Sprite tempSprite = null;
+        for (int i = 0; i < barrackFunc.UnitQueue.Length; i++)
+        {
+            if (barrackFunc.UnitQueue[i].activeInHierarchy)
+            {
+                continue;
+            }
+            else
+            {
+                switch (unitType)
+                {
+                    case UnitType.Sword:
+                        tempSprite = Resources.Load("Unit/BarrackUnit/Sword_Icon", typeof(Sprite)) as Sprite;
+                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[1], tempSprite));                        
+                        break;
+                    case UnitType.Bow:
+                        tempSprite = Resources.Load("Unit/BarrackUnit/Bow_Icon", typeof(Sprite)) as Sprite;
+                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[2], tempSprite));
+                        break;
+                    case UnitType.Javelin:
+                        tempSprite = Resources.Load("Unit/BarrackUnit/Spear_Icon", typeof(Sprite)) as Sprite;
+                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[3], tempSprite));
+                        break;
+                }
+
+                if(barrackFunc.UnitProductionCoroutine == null)
+                {
+                    barrackFunc.UnitProductionCoroutine = barrackFunc.StartCoroutine(barrackFunc.UnitProduction());
+                }
+                barrackFunc.UnitQueue[i].SetActive(true);
+                barrackFunc.UnitQueue[i].GetComponent<SpriteRenderer>().sprite = tempSprite;
+
+                break;
+            }
+        }
+    }
+
+}
+
+
