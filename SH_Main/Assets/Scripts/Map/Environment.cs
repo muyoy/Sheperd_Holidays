ce8114d7429546399367c518d2b40343c2f6a9b9
tpdms025 <45121963+tpdms025@users.noreply.github.com>
2021-01-12 01:23:35 +0900

    달 회전 연출 수정

--- SH_Main/Assets/Scripts/Map/Environment.cs
+++ SH_Main/Assets/Scripts/Map/Environment.cs
@@ -61,6 +61,8 @@ public class Environment : MonoBehaviour
     {
         //하늘 회전
         StartCoroutine(sky.Rotation(-180.0f, duration));
+        //달 회전
+        StartCoroutine(planet.MoveEllipse(-90.0f, 90.0f, duration));
 
         yield return new WaitForSeconds(duration * 0.5f);
 
@@ -78,13 +80,14 @@ public class Environment : MonoBehaviour
         //하늘 회전
         StartCoroutine(sky.Rotation(0.0f, duration));
         //달 회전
-        StartCoroutine(planet.MoveEllipse(-90.0f, 90.0f, duration));
+        StartCoroutine(planet.MoveEllipse(-90.0f, -45.0f, 2.0f));
 
         yield return new WaitForSeconds(duration * 0.5f);
 
         //산 변경
         mountian_renderer.sprite = nightMountainImage;
         //TODO: 건물 변경
+        //
     }
 
 }
