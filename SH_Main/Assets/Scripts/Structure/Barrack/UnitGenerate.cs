c2009286a4ad92b3890cc8d1f2bfcf957ce76107
CorgiMuzi <49934162+CorgiMuzi@users.noreply.github.com>
2021-01-12 12:15:51 +0900

    UnitGenerate_Backup

--- SH_Main/Assets/Scripts/Structure/Barrack/UnitGenerate.cs
+++ SH_Main/Assets/Scripts/Structure/Barrack/UnitGenerate.cs
@@ -16,13 +16,14 @@ public struct GenUnitInfo{
 
 public class UnitGenerate : MonoBehaviour
 {
-    public enum UnitType { Sword = 0, Bow, Javelin }
+    public enum UnitType { Assasin =0,Sword , Javelin, Bow, Wizard }
     private Barrack barrackFunc;
+    private GameManager GM;
     public UnitType unitType;
 
-    private void Awake()
+    private void Start()
     {
-
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     public void TargetBarrack(GameObject barrack) // 배럭이 생성되거나 파괴되고 난 후에 다시 배럭을 지정해줘야 할 때 사용
@@ -43,28 +44,54 @@ public class UnitGenerate : MonoBehaviour
             {
                 switch (unitType)
                 {
+                    case UnitType.Assasin:
+                        if (GM.money < DBManager.instance.SheepDatas[0].cost) { break; }
+                        tempSprite = Resources.Load("Unit/BarrackUnit/thief_Icon", typeof(Sprite)) as Sprite;
+                        GM.money -= DBManager.instance.SheepDatas[0].cost;
+                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[0], tempSprite));
+                        break;
+
                     case UnitType.Sword:
+                        if(GM.money < DBManager.instance.SheepDatas[1].cost) { break; }
                         tempSprite = Resources.Load("Unit/BarrackUnit/Sword_Icon", typeof(Sprite)) as Sprite;
+                        GM.money -= DBManager.instance.SheepDatas[1].cost;
                         barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[1], tempSprite));                        
                         break;
-                    case UnitType.Bow:
-                        tempSprite = Resources.Load("Unit/BarrackUnit/Bow_Icon", typeof(Sprite)) as Sprite;
-                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[3], tempSprite));
-                        break;
+
                     case UnitType.Javelin:
+                        if (GM.money < DBManager.instance.SheepDatas[2].cost) { break; }
                         tempSprite = Resources.Load("Unit/BarrackUnit/Spear_Icon", typeof(Sprite)) as Sprite;
+                        GM.money -= DBManager.instance.SheepDatas[2].cost;
                         barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[2], tempSprite));
                         break;
+
+                    case UnitType.Bow:
+                        if (GM.money < DBManager.instance.SheepDatas[3].cost) { break; }
+                        tempSprite = Resources.Load("Unit/BarrackUnit/Bow_Icon", typeof(Sprite)) as Sprite;
+                        GM.money -= DBManager.instance.SheepDatas[3].cost;
+                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[3], tempSprite));
+                        break;
+
+                    case UnitType.Wizard:
+                        if (GM.money < DBManager.instance.SheepDatas[4].cost) { break; }
+                        tempSprite = Resources.Load("Unit/BarrackUnit/wand_Icon", typeof(Sprite)) as Sprite;
+                        GM.money -= DBManager.instance.SheepDatas[4].cost;
+                        barrackFunc.GenUnitQueue.Enqueue(new GenUnitInfo(DBManager.instance.SheepDatas[4], tempSprite));
+                        break;
                 }
 
-                if(barrackFunc.UnitProductionCoroutine == null)
+                if(barrackFunc.GenUnitQueue.Count <= 0) { break; } else
                 {
-                    barrackFunc.UnitProductionCoroutine = barrackFunc.StartCoroutine(barrackFunc.UnitProduction());
-                }
-                barrackFunc.UnitQueue[i].SetActive(true);
-                barrackFunc.UnitQueue[i].GetComponent<SpriteRenderer>().sprite = tempSprite;
+                    if (barrackFunc.UnitProductionCoroutine == null)
+                    {
+                        barrackFunc.UnitProductionCoroutine = barrackFunc.StartCoroutine(barrackFunc.UnitProduction());
+                    }
+                    barrackFunc.UnitQueue[i].SetActive(true);
+                    barrackFunc.UnitQueue[i].GetComponent<SpriteRenderer>().sprite = tempSprite;
 
-                break;
+                    break;
+                }
+                
             }
         }
     }
